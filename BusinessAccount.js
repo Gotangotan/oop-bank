6e9577dc716fd7dfd706b71a962227820347999e
Tan Nguyen <tannguyen85@gmail.com>
2021-02-20 14:57:31 +0100

    Learning goals Practice writing a class Implementing a constructor on a class Adding methods to a class

--- BusinessAccount.js
+++ BusinessAccount.js
@@ -1,2 +1,30 @@
 // Export your class here as module.exports = MyClass
-module.exports = null;
+
+class BusinessAccount{
+
+    constructor(name, balance, creditLimit, sepaPermission) {
+        this.name = name;
+        this.balance = balance;
+        this.creditLimit = creditLimit;
+        this.sepaPermission = sepaPermission;
+    }
+    makeDeposit(amount){
+        if(amount > 0 ){
+            this.balance = this.balance + amount;
+        }
+    }
+    makeWithdrawal(amount){
+        if (amount < this.balance + this.creditLimit && amount < 20000 ){
+            this.balance = this.balance - amount
+        }
+    }
+    sepaInvoice(amount){
+        if (this.sepaPermission === true && amount > 0){
+            const transactionCost = 0.99
+            this.balance = this.balance + amount * transactionCost
+        }
+    }
+
+}
+// Amount passed to sepaInvoice should be added to balance minus 1% transaction cost
+module.exports = BusinessAccount;
